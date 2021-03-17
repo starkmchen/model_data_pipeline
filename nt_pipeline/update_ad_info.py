8e05df667000349fd7a179e1d1a7697612e112d7
chenmin <ll3021359>
2021-03-17 15:19:40 +0000

    add update ad info

---
+++ nt_pipeline/update_ad_info.py
@@ -0,0 +1,49 @@
+import mysql.connector
+import datetime
+import sys
+from store_table_pb2 import StoreAdInfo
+from model_ad_pb2 import AdInfo
+
+def get_mysql_result(host, sql, user, passwd):
+    import mysql.connector
+    db = mysql.connector.connect(
+        host = host,
+        user = user,
+        passwd = passwd)
+    cursor = db.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+def update_creative_info(store_ad_info):
+  host = "prod.cpi-platform-ro.ads.sg1.mysql"
+  user = "cpi_platform_reader"
+  passwd = "mEAYkZeJVQ7KjsMv8WKqWXumiehAhKCD"
+  before_day = (datetime.datetime.now() - datetime.timedelta(days = 7)).strftime("%Y-%m-%d %H:%M:%S")
+  sql = "select id,create_time from cpi_platform.creatives where create_time > STR_TO_DATE('%s', '%%Y-%%m-%%d %%H:%%i:%%s')" % before_day
+  result = get_mysql_result(host, sql, user, passwd)
+  for item in result:
+      c_id = item[0]
+      create_time = int(item[1].strftime("%s"))
+      ad_info = AdInfo()
+      ad_info.creative_create_time = create_time
+      key = "c_id#%s" % c_id
+      store_ad_info.ad_infos[key].CopyFrom(ad_info)
+
+
+def load_pkg_category(fname):
+  f = open(fname)
+  j = json.load(f)
+  a = StoreAdInfo()
+  json_format.Parse(json.dumps(j), a)
+  return a
+
+def main(argv):
+    store_ad_info = load_pkg_category(argv[0])
+    update_creative_info(store_ad_info)
+    f = open(argv[1], 'wb')
+    f.write(a.SerializeToString())
+    f.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ nt_pipeline/update_ad_info.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+now_hour=$(date -d "1 hour ago" +%Y%m%d%H)
+now_day=$(date -d "1 hour ago" +%Y%m%d)
+
+function download_pkg_category()
+{
+  fname=$(aws s3 ls s3://sprs.push.us-east-1.prod/data/warehouse/model/pkg_info/ | tail -n 1 | awk '{print $2}')
+  full_fname=s3://sprs.push.us-east-1.prod/data/warehouse/model/pkg_info/$fname
+  aws s3 sync $full_fname pkg_info_dir.${now_hour}
+  cat pkg_info_dir.${now_hour} > pkg_info.${now_hour}
+  rm -r pkg_info_dir.${now_hour}
+}
+
+function process()
+{
+  python update_ad_info.py pkg_info.${now_hour} pkg_info.pb.${now_hour}
+
+  ret=$?
+  alert $ret update_ad_info.py
+
+  if [ $ret -ne 0 ];then
+      return
+  fi
+
+  md5sum pkg_info.pb.${now_hour} | awk '{print $1}' > pkg_info.pb.${now_hour}.md5
+  aws s3 cp pkg_info.pb.${now_hour} s3://sprs-ads-sg1/pre/feature/pkg_info/$now_day/pkg_info.pb
+  aws s3 cp pkg_info.pb.${now_hour}.md5 s3://sprs-ads-sg1/pre/feature/pkg_info/$now_day/pkg_info.pb.md5
+  aws s3 cp pkg_info.pb.${now_hour} s3://sprs-ads-sg1/prod/feature/pkg_info/$now_day/pkg_info.pb
+  aws s3 cp pkg_info.pb.${now_hour}.md5 s3://sprs-ads-sg1/prod/feature/pkg_info/$now_day/pkg_info.pb.md5
+}
+
+
+function rmfile()
+{
+  rm pkg_info.pb.${now_hour}
+  rm pkg_info.${now_hour}
+}
+
+
+download_pkg_category
+process
+rmfile
